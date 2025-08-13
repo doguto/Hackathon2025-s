ba1f05c4868e33ffe5104d794f9a51a3a6cdd6a9
doguto <doguto110@gmail.com>
2025-08-14 01:30:01 +0900

    Test: MasterMemoryによるデータのバイナリ化をテスト

--- Assets/Project/Scripts/Repository/Schema/TestSchema.cs
+++ Assets/Project/Scripts/Repository/Schema/TestSchema.cs
@@ -6,6 +6,12 @@ namespace Project.Scripts.Repository.Schema
     [MemoryTable("Test"), MessagePackObject(true)]
     public sealed class TestSchema
     {
+        public TestSchema(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        
         [PrimaryKey]
         public int Id { get; set; }
         public string Name { get; set; }
