947e3ce54deffb5715d287839b1336c66fa6f9a5
doguto <doguto110@gmail.com>
2025-08-13 17:31:47 +0900

    Feat: Schemaファイルの設定

---
+++ Assets/Project/Scripts/Repository/Schema/TestSchema.cs
@@ -0,0 +1,13 @@
+﻿using MasterMemory;
+using MessagePack;
+
+namespace Project.Scripts.Repository.Schema
+{
+    [MemoryTable("Test"), MessagePackObject(true)]
+    public sealed class TestSchema
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
