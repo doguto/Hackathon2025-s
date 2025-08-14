65400146709e24a0daf280a311678e18f1427338
doguto <doguto110@gmail.com>
2025-08-14 22:16:40 +0900

    Feat: MonoPresenterの改良

--- Assets/Project/Scripts/Presenter/MonoPresenter.cs
+++ Assets/Project/Scripts/Presenter/MonoPresenter.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 namespace Project.Scripts.Presenter
 {
-    public class MonoPresenter : MonoBehaviour
+    public abstract class MonoPresenter : MonoBehaviour
     {
-        protected virtual void Init(){}
+        protected virtual void Awake()
+        {
+            Init();
+            Debug.Log($"Initialized {name}", this);
+        }
+        
+        protected abstract void Init();
     }
 }
