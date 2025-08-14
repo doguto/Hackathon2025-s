ce1df4f083c3f9c98121cf9d6429c08b68fdd5a7
doguto <doguto110@gmail.com>
2025-08-14 21:22:53 +0900

    Feat: GameDatabaseのセットアップ処理を作成

---
+++ Assets/Project/Scripts/Infra/GameDatabase.cs
@@ -0,0 +1,16 @@
+﻿using MasterMemory;
+
+namespace Project.Scripts.Infra
+{
+    public static class GameDatabase
+    {
+        public static MemoryDatabase MasterDatabase { get; private set; }
+        public static MemoryDatabase UserDatabase { get; private set; }
+
+        public static void SetDatabase(MemoryDatabase masterDatabase, MemoryDatabase userDatabase)
+        {
+            MasterDatabase = masterDatabase;
+            UserDatabase = userDatabase;
+        }
+    }
+}
---
+++ Assets/Project/Scripts/Infra/GameDatabaseLauncher.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using MasterMemory;
+using MessagePack;
+using MessagePack.Resolvers;
+using UnityEditor;
+using UnityEngine;
+
+namespace Project.Scripts.Infra
+{
+    public class GameDatabaseLauncher
+    {
+        const string BinaryDirectoryPath = "Assets/Project/DataStore/Binary";
+
+        const string MasterDataBinaryName = "MasterData.bytes";
+        const string UserDataBinaryName = "UserData.bytes";
+        
+        string MasterDataBinaryPath => Path.Combine(BinaryDirectoryPath, MasterDataBinaryName);
+        string UserDataBinaryPath => Path.Combine(BinaryDirectoryPath, UserDataBinaryName);
+        
+        public void Launch()
+        {
+            // MessagePackの初期化
+            var messagePackResolvers = CompositeResolver.Create(
+                MasterMemoryResolver.Instance,
+                StandardResolver.Instance
+            );
+            var options = MessagePackSerializerOptions.Standard.WithResolver(messagePackResolvers);
+            MessagePackSerializer.DefaultOptions = options;
+
+            var masterDatabase = CreateMasterDatabase();
+            var userDatabase = CreateUserDatabase();
+            
+            // GameDatabaseの初期化
+            GameDatabase.SetDatabase(masterDatabase, userDatabase);
+        }
+
+        MemoryDatabase CreateMasterDatabase()
+        {
+            var masterBinaryAsset = AssetDatabase.LoadAssetAtPath<TextAsset>(MasterDataBinaryPath);
+            var masterBinary = masterBinaryAsset.bytes;
+            return new MemoryDatabase(masterBinary);
+        }
+
+        MemoryDatabase CreateUserDatabase()
+        {
+            var userBinaryAsset = AssetDatabase.LoadAssetAtPath<TextAsset>(UserDataBinaryPath);
+            var userBinary = userBinaryAsset.bytes;
+            return new MemoryDatabase(userBinary);
+        }
+    }
+}
