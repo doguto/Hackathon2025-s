cc2dbd14ff598bb21900b8c39167d4462397f604
doguto <doguto110@gmail.com>
2025-08-15 14:48:37 +0900

    Feat: ModelとViewの接続

--- Assets/Project/Scenes/Sample/Scripts/Presenter/SampleScenePresenter.cs
+++ Assets/Project/Scenes/Sample/Scripts/Presenter/SampleScenePresenter.cs
@@ -1,13 +1,20 @@
 ﻿using Project.Scenes.Sample.Scripts.Model;
+using Project.Scenes.Sample.Scripts.View;
 using Project.Scripts.Presenter;
+using UnityEngine;
 
 namespace Project.Scenes.Sample.Scripts.Presenter
 {
     public class SampleScenePresenter : MonoPresenter
     {
+        [SerializeField] TestObjectView testObjectView;
+
         protected override void Init()
         {
             var sceneModel = new SampleSceneModel();
+
+            testObjectView.Init();
+            testObjectView.Setup(sceneModel.Id, sceneModel.Name);
         }
     }
 }
