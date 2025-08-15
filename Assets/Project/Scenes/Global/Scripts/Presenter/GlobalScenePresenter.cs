b0c45cc442dfdcbdc8b3d751d7b394a00cdd0d29
doguto <doguto110@gmail.com>
2025-08-15 14:48:19 +0900

    Feat: GlobalScenePresenterを必ず最初に実行するように

--- Assets/Project/Scenes/Global/Scripts/Presenter/GlobalScenePresenter.cs
+++ Assets/Project/Scenes/Global/Scripts/Presenter/GlobalScenePresenter.cs
@@ -1,13 +1,20 @@
 using Project.Scenes.Global.Scripts.Model;
 using Project.Scripts.Presenter;
+using UnityEngine;
 
 namespace Project.Scenes.Global.Scripts.Presenter
 {
     public class GlobalScenePresenter : MonoPresenter
     {
-        protected override void Init()
+        protected override void Init() { }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        protected static void Initialize()
         {
             var sceneModel = new GlobalSceneModel();
+#if UNITY_EDITOR
+            Debug.Log("Initializes GlobalScenePresenter");
+#endif
         }
     }
 }
