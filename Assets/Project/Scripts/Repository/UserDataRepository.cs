075b3bf48e95370c67f3a4ebfd6f46ac1bdb2ec4
doguto <doguto110@gmail.com>
2025-08-16 11:36:15 +0900

    Refactor: データの取得をRepositoryを介さないように変更

--- Assets/Project/Scripts/Repository/UserDataRepository.cs
+++ Assets/Project/Scripts/Repository/UserDataRepository.cs
@@ -3,7 +3,7 @@ using Project.Scripts.Infra;
 
 namespace Project.Scripts.Repository
 {
-    public class IUserDataRepository : IDataRepository
+    public class UserDataRepository : IDataRepository
     {
         public MemoryDatabase Database => GameDatabase.UserDatabase;
     }
